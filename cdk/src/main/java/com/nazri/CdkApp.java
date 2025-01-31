b9f99437222283f04bb3084642210a6247cc027f
649000 <649000@users.noreply.github.com>
2025-01-31 19:48:37 +0800

    CDK for AWS API Gateway

--- cdk/src/main/java/com/nazri/CdkApp.java
+++ cdk/src/main/java/com/nazri/CdkApp.java
@@ -1,6 +1,9 @@
 package com.nazri;
 
+import com.nazri.config.StackConfig;
+import com.nazri.stack.APIGatewayStack;
 import com.nazri.stack.CdkStack;
+import com.nazri.util.Constant;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.StackProps;
 
@@ -8,33 +11,40 @@ public class CdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new CdkStack(app, "CdkStack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+        String environment = Constant.DEV;
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
-                .env(Environment.builder()
-                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                        .region(System.getenv("CDK_DEFAULT_REGION"))
-                        .build())
-                */
 
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
-                        .build())
-                */
+        StackConfig stackConfig = getStackConfig(environment);
 
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                .build());
+        new APIGatewayStack(app, "gratitudejar-api-stack", stackConfig.getStackProps()
+                .stackName("gratitudejar-api-stack")
+                .description("HTTP API Gateway Stack for Gratitude Jar")
+                .build(),
+                stackConfig,
+                null);
 
         app.synth();
+    }
+
+    public static StackConfig getStackConfig(String environment) {
+        StackConfig stackConfig;
+        switch (environment) {
+            case Constant.SIT:
+                stackConfig = new StackConfig.Builder()
+                        .withEnvironment(Constant.SIT)
+                        .build();
+                break;
+            case Constant.UAT:
+                stackConfig = new StackConfig.Builder()
+                        .withEnvironment(Constant.UAT)
+                        .build();
+                break;
+            default:
+                stackConfig = new StackConfig.Builder()
+                        .withEnvironment(Constant.DEV)
+                        .build();
+        }
+        return stackConfig;
     }
 }
 
