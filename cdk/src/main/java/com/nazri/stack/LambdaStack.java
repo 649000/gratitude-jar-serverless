d00c75f6890d9be971dc3818366d4e157cf4131a
649000 <649000@users.noreply.github.com>
2025-02-01 18:34:27 +0800

    Setup Lambda Stack for Quarkus REST API

--- cdk/src/main/java/com/nazri/stack/LambdaStack.java
+++ cdk/src/main/java/com/nazri/stack/LambdaStack.java
@@ -1,6 +1,7 @@
 package com.nazri.stack;
 
 import com.nazri.config.StackConfig;
+import com.nazri.util.Constant;
 import com.nazri.util.TagUtil;
 import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Stack;
@@ -27,15 +28,18 @@ public class LambdaStack extends Stack {
     }
 
     private Function createNonNativeApiFunction() {
-        return Function.Builder.create(this, "gratitudejar-jwt-auth")
-                .functionName("gratitudejar-jwt-auth")
-                .description("Gratitude Jar: JWT Authoriser")
-                .code(Code.fromAsset("../quarkus-lambda/target/function.zip"))
-                .timeout(Duration.seconds(300))
+        return Function.Builder.create(this, "gratitudejar-api-lambda")
+                .description("Gratitude Jar: REST API")
+                .code(Code.fromAsset("../api/target/function.zip"))
+                .timeout(Duration.seconds(15))
                 .memorySize(512)
                 .runtime(Runtime.JAVA_21)
                 .handler("io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler::handleRequest")
                 .environment(Map.of(
+                        "quarkus_lambda_handler", "io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler::handleRequest",
+                        "quarkus_datasource_jdbc_url", Constant.SUPABASE_DB_URL,
+                        "quarkus_datasource_username", Constant.SUPABASE_DB_USER,
+                        "quarkus_datasource_password", Constant.SUPABASE_DB_PASSWORD,
                         // For JVM only
                         "JAVA_TOOL_OPTIONS", "-XX:+TieredCompilation -XX:TieredStopAtLevel=1"
                 ))
